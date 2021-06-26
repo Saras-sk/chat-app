670d486c308412efeee3e6abfdd8b3d797162298
Sarastk2121@gmail.com <Sarastk2121@gmail.com>
2021-06-26 08:23:05 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+        height="100vh"
+        projectID="98392d38-f03f-4517-8c18-2eb87a639f82"
+        userName="Saras's JSM"
+        userSecret="12341234"
+        />
+    );
+}
+
+export default App; 
